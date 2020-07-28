7fd1bc93b770922dacc919e958ed06bce0c74e9d
elaine <alien@192.168.7.104>
2020-07-28 14:20:23 +0800

    set statuscode

--- index.js
+++ index.js
@@ -16,10 +16,18 @@
 //     console.log(err.stack);
 // });
 
-
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/message' && method === 'GET') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>my first page</title></head>')
