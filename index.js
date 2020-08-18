f1d2fcadf68bf4680e2e182c2fa418f412f445ed
alieeeeeeeen <elainechen77@icloud.com>
2020-08-18 16:56:54 +0800

    add use middleware

--- index.js
+++ index.js
@@ -2,17 +2,25 @@
 const http = require('http');
 const routes = require('./routes');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next()
+app.use(bodyParser.urlencoded());
+
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/> <button type="submit">submit</button></form>')
 })
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     console.log('in the other middleware');
-    res.send('hello from express')
+    res.send('<h1>hello from express</h1>')
 })
 
 const server = http.createServer(app)
