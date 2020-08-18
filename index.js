fa8bb3c33214226d3ec459a9b96ea05a22ecfbdb
alieeeeeeeen <elainechen77@icloud.com>
2020-08-18 15:01:46 +0800

    add express

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 
 const http = require('http');
 const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes)
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('in the other middleware');
+    res.send('hello from express')
+})
+
+const server = http.createServer(app)
 
 server.listen(3000);
