27ddd07ee71c13adcac3117c5f22c0766c15dd14
alieeeeeeeen <elainechen77@icloud.com>
2020-08-25 09:02:55 +0800

    serve static files

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -21,7 +21,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
-
 
 const server = http.createServer(app)
 
